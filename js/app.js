14e01150ac2b6531c96afb36d09cb0359c0ba3cb
AnthonyCorrado <amcstudios@yahoo.com>
2014-08-08 18:55:13 -0700

    added jquery image scaling and base styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('SeatMap', ['mm.foundation']);
+
+var screenWidth = $(window).width();
+var picHeight = screenWidth * 0.6686217; // maintains image aspect ratio
+console.log(picHeight);
+console.log(screenWidth);
+
+$(window).load(function() {
+	//sets the image to full width of device while maintaining proper aspec ratio
+	$('.image-box').css({width: screenWidth, height: picHeight});
+});
+
